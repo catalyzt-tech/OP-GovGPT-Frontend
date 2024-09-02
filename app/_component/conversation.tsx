eeabf6f6cb987113711fe726a41c5c90086e0e0a
tgrziminiar <mixzazazaza1212@gmail.com>
2024-09-02 12:30:58 +0700

    comment some unused code for now

--- app/_component/conversation.tsx
+++ app/_component/conversation.tsx
@@ -1,4 +1,4 @@
-import MessageCard from "@/components/messaga-card";
+import MessageCard from "@/components/message-card";
 import React from "react";
 import { MessageType } from "../Cpage";
 import { Skeleton } from "@nextui-org/react";
@@ -11,65 +11,65 @@ type ConversationType = {
     isLoading: boolean
 }
 
-export const assistantMessages = [
-    <div key="1">
-      <p className="mb-3">
-        Certainly! Here&apos;s a summary of five creative ways to use your kids&apos; art:
-      </p>
-      <ol className="space-y-2">
-        <li>
-          <strong>Create Art Books:</strong> Turn scanned artwork into custom photo books.
-        </li>
-        <li>
-          <strong>Set Up a Gallery Wall:</strong> Use a dedicated wall with interchangeable frames for
-          displaying art.
-        </li>
-        <li>
-          <strong>Make Functional Items:</strong> Print designs on items like pillows, bags, or mugs.
-        </li>
-        <li>
-          <strong>Implement an Art Rotation System:</strong> Regularly change the displayed art,
-          archiving the older pieces.
-        </li>
-        <li>
-          <strong>Use as Gift Wrap:</strong> Repurpose art as unique wrapping paper for presents.
-        </li>
-      </ol>
-    </div>,
-    <div key="2">
-      <p className="mb-3">
-        Of course! Here are five more creative suggestions for what to do with your children&apos;s
-        art:
-      </p>
-      <ol className="space-y-2">
-        <li>
-          <strong>Create a Digital Archive:</strong> Scan or take photos of the artwork and save it in
-          a digital cloud storage service for easy access and space-saving.
-        </li>
-        <li>
-          <strong>Custom Calendar:</strong> Design a custom calendar with each month showcasing a
-          different piece of your child&apos;s art.
-        </li>
-        <li>
-          <strong>Storybook Creation:</strong> Compile the artwork into a storybook, possibly with a
-          narrative created by your child, to make a personalized book.
-        </li>
-        <li>
-          <strong>Puzzle Making:</strong> Convert their artwork into a jigsaw puzzle for a fun and
-          unique pastime activity.
-        </li>
-        <li>
-          <strong>Home Decor Items:</strong> Use the artwork to create home decor items like coasters,
-          magnets, or lampshades to decorate your house.
-        </li>
-      </ol>
-    </div>,
-  ];
+// export const assistantMessages = [
+//     <div key="1">
+//       <p className="mb-3">
+//         Certainly! Here&apos;s a summary of five creative ways to use your kids&apos; art:
+//       </p>
+//       <ol className="space-y-2">
+//         <li>
+//           <strong>Create Art Books:</strong> Turn scanned artwork into custom photo books.
+//         </li>
+//         <li>
+//           <strong>Set Up a Gallery Wall:</strong> Use a dedicated wall with interchangeable frames for
+//           displaying art.
+//         </li>
+//         <li>
+//           <strong>Make Functional Items:</strong> Print designs on items like pillows, bags, or mugs.
+//         </li>
+//         <li>
+//           <strong>Implement an Art Rotation System:</strong> Regularly change the displayed art,
+//           archiving the older pieces.
+//         </li>
+//         <li>
+//           <strong>Use as Gift Wrap:</strong> Repurpose art as unique wrapping paper for presents.
+//         </li>
+//       </ol>
+//     </div>,
+//     <div key="2">
+//       <p className="mb-3">
+//         Of course! Here are five more creative suggestions for what to do with your children&apos;s
+//         art:
+//       </p>
+//       <ol className="space-y-2">
+//         <li>
+//           <strong>Create a Digital Archive:</strong> Scan or take photos of the artwork and save it in
+//           a digital cloud storage service for easy access and space-saving.
+//         </li>
+//         <li>
+//           <strong>Custom Calendar:</strong> Design a custom calendar with each month showcasing a
+//           different piece of your child&apos;s art.
+//         </li>
+//         <li>
+//           <strong>Storybook Creation:</strong> Compile the artwork into a storybook, possibly with a
+//           narrative created by your child, to make a personalized book.
+//         </li>
+//         <li>
+//           <strong>Puzzle Making:</strong> Convert their artwork into a jigsaw puzzle for a fun and
+//           unique pastime activity.
+//         </li>
+//         <li>
+//           <strong>Home Decor Items:</strong> Use the artwork to create home decor items like coasters,
+//           magnets, or lampshades to decorate your house.
+//         </li>
+//       </ol>
+//     </div>,
+//   ];
   
-export const userMessages = [
-"What are 5 creative things I could do with my kids' art? I don't want to throw them away, but it's also so much clutter.",
-"I didn't like the suggestions. Can you give me some more?",
-];
+// export const userMessages = [
+// "What are 5 creative things I could do with my kids' art? I don't want to throw them away, but it's also so much clutter.",
+// "I didn't like the suggestions. Can you give me some more?",
+// ];
 
 export default function Conversation({prompt, messages, isLoading}:ConversationType) {
   
